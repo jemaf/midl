c85cf70e396ff5e2794e627a8d040a25fc6aa458
edumontandon <edumontandon@31c26e86-03f9-e4e1-e341-10c45fc16455>
2011-06-18 23:32:30 +0000

    implementacao textbox

---
+++ midl/src/components/TextBox.java
@@ -0,0 +1,63 @@
+package components;
+
+import types.COMPONENT_TYPE;
+import types.INPUT_TYPE;
+
+public class TextBox extends Component {
+
+	
+	private String text;
+	private INPUT_TYPE inputType;
+	
+	public TextBox() {
+		this.componentType = COMPONENT_TYPE.TEXTBOX;
+		this.inputType = INPUT_TYPE.TEXTPERSONNAME;
+	}
+	
+	public TextBox(String id) {
+		this.componentType = COMPONENT_TYPE.TEXTBOX;
+		this.id = id;
+		this.inputType = INPUT_TYPE.TEXTPERSONNAME;
+	}
+
+	@Override
+	public void setSubComponent(Component c) {
+		this.subComponents.add(c);
+		
+	}
+
+	@Override
+	public Component getSubComponent(int pos) {
+		return this.subComponents.get(pos);
+	}
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * @return the inputType
+	 */
+	public INPUT_TYPE getInputType() {
+		return inputType;
+	}
+
+	/**
+	 * @param inputType the inputType to set
+	 */
+	public void setInputType(INPUT_TYPE inputType) {
+		this.inputType = inputType;
+	}
+	
+	
+}
